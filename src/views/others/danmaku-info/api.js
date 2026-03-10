64f75227e5ff36a58af2025fa26756b160a4056a
zxc7563598 <junjie.he.925@gmail.com>
2026-03-10 20:19:12 +0800

    fix(api): 为导出功能添加超时设置，确保请求稳定性

--- src/views/others/danmaku-info/api.js
+++ src/views/others/danmaku-info/api.js
@@ -2,5 +2,5 @@ import { request } from '@/utils'
 
 export default {
   read: (params = {}) => request.post('/others/danmaku-info/get-data', params), // 获取列表数据
-  export: (params = {}) => request.post('/others/danmaku-info/export-data', params, { responseType: 'blob' }), // 导出列表数据
+  export: (params = {}) => request.post('/others/danmaku-info/export-data', params, { responseType: 'blob', timeout: 60000 }), // 导出列表数据
 }
--- src/views/others/gift-blind-box/api.js
+++ src/views/others/gift-blind-box/api.js
@@ -2,6 +2,6 @@ import { request } from '@/utils'
 
 export default {
   read: (params = {}) => request.post('/others/gift-blind-box/get-data', params), // 获取列表数据
-  export: (params = {}) => request.post('/others/gift-blind-box/export-data', params, { responseType: 'blob' }), // 导出列表数据
+  export: (params = {}) => request.post('/others/gift-blind-box/export-data', params, { responseType: 'blob', timeout: 60000 }), // 导出列表数据
   getStatisticData: (uid, uname, create_date) => request.post('/others/gift-blind-box/get-statistic-data', { uid, uname, create_date }), // 获取列表数据
 }
